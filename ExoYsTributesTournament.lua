fd1991e73b3d45a43d778cd49f71af284a17302c
exoy94 <exoy@mail.de>
2022-12-22 21:15:21 +0100

    Basic Concept Notes

--- ExoYsTributesTournament.lua
+++ ExoYsTributesTournament.lua
@@ -1,1 +1,48 @@
+--[[ -------------]]
+--[[ --- Notes ---]]
+--[[ -------------]]
 
+-- A Tournament as a Object, containing 
+-- + playerLists
+-- + tournament type 
+-- + rules etc 
+
+-- Chat Handler to Read and Prepare Messages 
+
+-- Phases: 
+-- 1) "Open Tournament and set AutoInvite" (at first all sizes work with this, add LibDataShare later)
+-- 2) Players can invite themself / or be added (Do you get @name from people in chat in all cases? ) 
+-- 3) Define Rounds to play   <---|
+-- 4) Record Outcomes  -----------|
+-- 5) Output Rankings
+
+
+
+TributesTournament = TributesTournament or {}
+
+local ToTT = TributesTournament
+local EM = GetEventManager() 
+local WM = GetWindowManager()
+
+
+--[[ ------------------ ]]
+--[[ --- Initialize --- ]]
+--[[ ------------------ ]]
+
+local function Initialize() 
+
+    -- Gui window like LGM
+
+
+
+end 
+
+
+local function OnAddonLoaded(_, addonName) 
+    if addonName = then 
+        Initialize() 
+        -- Unregister Event 
+    end 
+end
+
+-- Register Event
